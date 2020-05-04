94513aa57e37be42b1e1f522fc397a29fc137607
pablo <peropaquepo@gmail.com>
2020-05-04 23:14:26 +0200

    django tutorial step 2

--- django_project/polls/views.py
+++ django_project/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.utils import timezone
 
 
 def index(request):
-    return HttpResponse('Hello world. Polls index.')
+    return HttpResponse('Hello world. Polls index. '+str(timezone.now()))
