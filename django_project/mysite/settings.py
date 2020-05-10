054be6e5ca4764d820b8c8ef087c936cc1afde51
Pablo Huentelemu <pablo.huentelemu@gmail.com>
2020-05-10 15:29:58 +0200

    start puzzleprinter app

--- django_project/mysite/settings.py
+++ django_project/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'puzzleprinter.apps.PuzzleprinterConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
