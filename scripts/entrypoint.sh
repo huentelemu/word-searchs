f1d8757e2b5be528d36725d51ff846a36398dc82
pablo <peropaquepo@gmail.com>
2020-07-15 16:07:02 +0200

    ensure admin user command

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')"
+python manage.py ensure_adminuser --username=admin \
+    --email=admin@example.com \
+    --password=pass
 
 uwsgi --socket :8000 --master --enable-threads --module mysite.wsgi
