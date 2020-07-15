8dea48e632da768ced49e7b9ccab80585e52e60b
pablo <peropaquepo@gmail.com>
2020-07-15 14:48:49 +0200

    another way

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput --username admin --email admin@example.com --password supah
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')"
 
 uwsgi --socket :8000 --master --enable-threads --module mysite.wsgi
