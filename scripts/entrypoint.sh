fef14a7f523a52b63336a752c8f32fc8212e77b2
pablo <peropaquepo@gmail.com>
2020-07-15 14:32:49 +0200

    add admin user

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser --noinput --username admin --email admin@example.com
 
 uwsgi --socket :8000 --master --enable-threads --module mysite.wsgi
